45d2e4e4042e2368dc7584a33757b4e62184312c
Swetabh333 <swetabh333@gmail.com>
2023-09-10 01:01:35 +0530

    hm

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary py-3 bg-primary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Form</a>
+    <a className="navbar-brand" href="/">Canara Bank</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
